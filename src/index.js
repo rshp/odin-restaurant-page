bf4f9dd6ccbe713369dc536566c967c5d09201a7
shpr <roman.shpr@gmail.com>
2022-04-05 18:52:19 +0300

    Add placeholder css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Iconify from '@iconify/iconify';
 import createContent from './homePage';
+import './style.css';
+
 const contentContainer = document.querySelector('.content');
 console.log(contentContainer);
 createContent(contentContainer);
