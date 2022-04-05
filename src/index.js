f621544aa6ae3e7732259d49cad31835664fa5aa
shpr <roman.shpr@gmail.com>
2022-04-05 22:50:02 +0300

    Add icon list and attempt icon generation

--- src/index.js
+++ src/index.js
@@ -5,3 +5,25 @@ import './style.css';
 const contentContainer = document.querySelector('.content');
 console.log(contentContainer);
 createContent(contentContainer);
+
+const backgroundContainer = document.createElement('div');
+backgroundContainer.classList.add('background-container');
+let iconNameList = [
+	'mdi:baguette',
+	'mdi:candycane',
+	'mdi:cheese',
+	'mdi:egg-fried',
+	'mdi:food-drumstick-outline',
+	'mdi:food-apple',
+	'mdi:hamburger',
+	'mdi:sausage',
+	'mdi:popcorn',
+];
+
+iconNameList.forEach((icon) => {
+	let iconElement = document.createElement('span');
+	iconElement.classList.add('iconify-inline');
+	iconElement.dataset.icon = icon;
+	backgroundContainer.appendChild(iconElement);
+});
+document.querySelector('body').appendChild(backgroundContainer);
